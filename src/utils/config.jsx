029ca6bd199d457e1fed361227dd55226607e126
eka <21106050023@student.uin-suka.ac.id>
2024-04-11 20:30:33 +0700

    fix: config

--- src/utils/config.jsx
+++ src/utils/config.jsx
@@ -1,5 +1,5 @@
 const config = {
-    BASE_URL: "http://localhost:5555",
+    BASE_URL: "https://babungkus-api.vercel.app/",
     IMAGE_BASE_URL: "http://localhost:5555/public/assets/images", // Ganti dengan URL base gambar kamu
 };
   
